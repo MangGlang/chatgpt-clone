c38c6e9702ca3d762de41fa77b094d592e06832b
MangGlang <71730799+MangGlang@users.noreply.github.com>
2023-07-26 02:16:33 -0700

    User Authentication - Log/Signout

---
+++ components/SessionProvider.tsx
@@ -0,0 +1,20 @@
+// Client Side Component
+"use client"
+
+import { Session } from "next-auth";
+import { SessionProvider as Provider} from "next-auth/react"
+import React from "react";
+
+// Type definitions
+type Props = {
+    children: React.ReactNode;
+    session: Session | null;
+}
+
+export function SessionProvider({ children, session }: Props ) {
+    return (
+        <Provider>
+            {children}
+        </Provider>
+    )
+}

f28f8311de5906211c08635adbfbde689115d33d
MangGlang <71730799+MangGlang@users.noreply.github.com>
2023-07-27 00:08:16 -0700

    Update

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -8,17 +8,21 @@ import { addDoc, collection } from "firebase/firestore";
 import { db } from "@/firebase";
 import { toast } from "react-hot-toast";
 
+// Making API call
+// 1. Adding to firebase from client
+// 2. Client then queries our own API
+
 type Props = {
-    chatId: string;
-}
+  chatId: string;
+};
 
-function ChatInput( { chatId}: Props) {
-    // Using state inside a component (server component), must use client side 
-const [prompt, setPrompt] = useState("");
-const { data: session } = useSession();
+function ChatInput({ chatId }: Props) {
+  // Using state inside a component (server component), must use client side
+  const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
 
-    // TODO: useSWR to get model
-    const model = "davinci";
+  // TODO: useSWR to get model
+  const model = "davinci";
 
   const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -29,72 +33,81 @@ const { data: session } = useSession();
     setPrompt("");
 
     const message: Message = {
-        text: input,
-        createdAt: serverTimestamp(),
-        user: {
-            _id: session?.user?.email!,
-            name: session?.user?.name!,
-            avatar: session?.user?.image! || `
-            https://ui-avatars.com/api/?name=${session?.user?.name}` 
-        }
-    }
-
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar:
+          session?.user?.image! ||
+          `
+            https://ui-avatars.com/api/?name=${session?.user?.name}`,
+      },
+    };
 
     // Pushing message into firestore database
-    await addDoc(collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages'), {
-        message
-    })
+    await addDoc(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId,
+        "messages"
+      ),
+        message,
+    );
 
     // Toast notification to say loading..
-    const notification = toast.loading('ChatGPT is thinking...');
+    const notification = toast.loading("ChatGPT is thinking...");
 
-    // Fetch Method 
+    // Fetch Method
 
-    await fetch('/api/askQuestion', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            prompt: input, chatId, model, session
-        })
+    await fetch("/api/askQuestion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
     }).then(() => {
-        // Toast notification = successful!
-        toast.success('ChatGPT has responded!', {
-            id: notification,
-        })
-    })
-    
-};
+      // Toast notification = successful!
+      toast.success("ChatGPT has responded!", {
+        id: notification,
+      });
+    });
+  };
 
   return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
-        <form onSubmit={sendMessage} className="p-5 space-x-5 flex">
-            <input 
-            className="bg-transparent focus:outline-none flex-1
+      <form onSubmit={sendMessage} className="p-5 space-x-5 flex">
+        <input
+          className="bg-transparent focus:outline-none flex-1
             disabled:cursor-not-allowed disabled:text-gray-300"
-            disabled={!session}
-            // Upon changing search bar value
-            value={prompt}
-            onChange={e => setPrompt(e.target.value)}
-            type="text" 
-            placeholder="Type your message here..."
-            />
-            <button 
-            disabled={!prompt || !session}
-            className="bg-[#11A37F] hover:opacity-50 text-white font-bold
+          disabled={!session}
+          // Upon changing search bar value
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text"
+          placeholder="Type your message here..."
+        />
+        <button
+          disabled={!prompt || !session}
+          className="bg-[#11A37F] hover:opacity-50 text-white font-bold
             px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
-            type="submit">
-                <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
-            </button>
-        </form>
+          type="submit"
+        >
+          <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+        </button>
+      </form>
 
-        <div>
-            {/* ModalSelection */}
-
-        </div>
+      <div>{/* ModalSelection */}</div>
     </div>
-  )
+  );
 }
 
-export default ChatInput
+export default ChatInput;
---
+++ pages/api/auth/askQuestion.ts
@@ -0,0 +1,60 @@
+// Next.js API 
+
+// Endpoint
+    // 1. Strip out values that are passed through via destructing chat input
+    // 2. Check to see if prompt details and chatId are available (if session is online)
+    // 3. Query chatGPT: packages chatGPT message as a response 
+    // 4. Access admin to connect backend of firebase?
+    // 5. return answer as text
+
+import admin from 'firebase-admin';
+import query from '@/lib/queryApi';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { adminDb } from '@/firebaseAdmin';
+
+type Data = {
+    answer: string
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+
+    // Destructing chat input 
+    const {prompt, chatId, model, session } = req.body;
+
+    if (!prompt) {
+        res.status(400).json({ answer: "Please provide a prompt!" })
+        return;
+    }
+
+    if (!chatId) {
+        res.status(400).json({ answer: "Please provide a valid chat ID!" })
+        return;
+    }
+
+    // ChatGPT Query
+    const response = await query(prompt, chatId, model)
+
+    const message: Message = {
+        text: response || "ChatGPT was unable to find an answer for that!",
+        // admin priv required for backend 
+        createdAt: admin.firestore.Timestamp.now(),
+        user: {
+            _id: 'ChatGPT',
+            name: 'ChatGPT',
+            avatar: "https://links.papareact.com/89k",
+        },
+    }
+
+    await adminDb
+    .collection('users')
+    .doc(session?.user?.email)
+    .collection("chats")
+    .doc(chatId)
+    .collection("messages")
+    .add(message);
+
+    res.status(200).json({ answer: message.text })
+}

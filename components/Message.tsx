911691bf28a4c5594be1d194ea3556c4eb7033aa
MangGlang <71730799+MangGlang@users.noreply.github.com>
2023-07-28 03:47:51 -0700

    IMAGE

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,5 +1,4 @@
 import { DocumentData } from "firebase/firestore";
-import Image from "next/image";
 
 type Props = {
   message: DocumentData;
@@ -11,7 +10,7 @@ function Message({ message }: Props) {
   return (
   <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}>
         <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
-            <Image src={message.user.avatar} alt="" className="h-8 w-8" />
+            <img src={message.user.avatar} alt="" className="h-8 w-8" />
             <p className="pt-1 text-sm">
                 {message.text}
             </p>
